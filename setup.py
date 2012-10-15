8c2348efc4d72db635278f5561da43025dd220c5
Jason Ish <ish@unx.ca>
2012-10-14 22:33:10 -0600

    Rename and use distutils to make it installable + cleanup.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+VERSION = "0.1"
+
+setup(name="imaparchive",
+      version=VERSION,
+      url="https://github.com/jasonish/imaparchive",
+      scripts=["imaparchive"],
+      )
